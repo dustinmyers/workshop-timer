c676b809d8e39d08bc6fd75e48b32c8185ef41c6
Dustin Myers <dustinmyers@users.noreply.github.com>
2019-05-15 12:35:10 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Particles from 'react-particles-js';
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 import './styles.css';
 
@@ -33,7 +33,11 @@ function App() {
   );
 }
 
-const end = moment('2019-03-07 02:00:00', 'YYYY-MM-DD kk:mm:ss');
+const end = moment.tz(
+  '2019-05-15 8:00:00',
+  'YYYY-MM-DD kk:mm:ss',
+  'American/Denver'
+);
 
 class Header extends React.Component {
   state = {
@@ -61,7 +65,7 @@ class Header extends React.Component {
       <div className="flex-container">
         <div className="header">
           <h2>{this.state.timer}</h2>
-          <h4>WEB17 React Hooks Workshop!</h4>
+          <h4>WEB19 React Hooks Workshop!</h4>
         </div>
       </div>
     );
